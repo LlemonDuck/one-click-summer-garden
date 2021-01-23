f9092f43ff780360445d4daa3bb25946da6db727
Paul Norton <napkinorton@gmail.com>
2021-01-23 17:07:50 -0500

    Update to 1.1 with stamina notifications

--- src/main/java/com/duckblade/runelite/summergarden/SummerGardenConfig.java
+++ src/main/java/com/duckblade/runelite/summergarden/SummerGardenConfig.java
@@ -1,60 +1,71 @@
 package com.duckblade.runelite.summergarden;
 
 import java.awt.Color;
+
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
-public interface SummerGardenConfig extends Config
-{
-	@ConfigItem(
-		keyName = "highlightGood",
-		name = "On-Parity Color",
-		description = "Color to highlight elementals whose parity is correct."
-	)
-	default Color highlightGood()
-	{
-		return Color.green.darker();
-	}
+@ConfigGroup("oneclicksummergarden")
+public interface SummerGardenConfig extends Config {
+    @ConfigItem(
+            keyName = "highlightGood",
+            name = "On-Parity Color",
+            description = "Color to highlight elementals whose parity is correct.",
+            position = 1
+    )
+    default Color highlightGood() {
+        return Color.green.darker();
+    }
 
-	@ConfigItem(
-		keyName = "highlightBad",
-		name = "Off-Parity Color",
-		description = "Color to highlight elementals whose parity is incorrect."
-	)
-	default Color highlightBad()
-	{
-		return Color.orange;
-	}
+    @ConfigItem(
+            keyName = "highlightBad",
+            name = "Off-Parity Color",
+            description = "Color to highlight elementals whose parity is incorrect.",
+            position = 2
+    )
+    default Color highlightBad() {
+        return Color.orange;
+    }
 
-	@ConfigItem(
-		keyName = "highlightLaunch",
-		name = "Launch Color",
-		description = "Color to highlight elementals when it is time to click the Sq'irk tree."
-	)
-	default Color highlightLaunch()
-	{
-		return Color.decode("#00ADFF");
-	}
+    @ConfigItem(
+            keyName = "highlightLaunch",
+            name = "Launch Color",
+            description = "Color to highlight elementals when it is time to click the Sq'irk tree.",
+            position = 3
+    )
+    default Color highlightLaunch() {
+        return Color.decode("#00ADFF");
+    }
 
-	@ConfigItem(
-		keyName = "highlightLaunchTile",
-		name = "Highlight Launch Tile",
-		description = "Whether or not to highlight the tile at which the first elemental will be when you click the Sq'irk tree."
-	)
-	default boolean highlightLaunchTile()
-	{
-		return true;
-	}
+    @ConfigItem(
+            keyName = "highlightLaunchTile",
+            name = "Highlight Launch Tile",
+            description = "Whether or not to highlight the tile at which the first elemental will be when you click the Sq'irk tree.",
+            position = 4
+    )
+    default boolean highlightLaunchTile() {
+        return true;
+    }
 
-	@ConfigItem(
-		keyName = "showNumbers",
-		name = "Show Numbers",
-		description = "Whether to show numbers on Elementals, showing how many resets will be needed."
-	)
-	default ShowNumbers showNumbers()
-	{
-		return ShowNumbers.YES;
-	}
+    @ConfigItem(
+            keyName = "showNumbers",
+            name = "Show Numbers",
+            description = "Whether to show numbers on Elementals, showing how many resets will be needed.",
+            position = 5
+    )
+    default ShowNumbers showNumbers() {
+        return ShowNumbers.YES;
+    }
+
+    @ConfigItem(
+            keyName = "staminaWarning",
+            name = "Low Stamina Threshold",
+            description = "What stamina level to warn to use stamina potion (0 to disable).",
+            position = 6
+    )
+    default int staminaThreshold() {
+        return 25;
+    }
+
 }
--- src/main/java/com/duckblade/runelite/summergarden/SummerGardenPlugin.java
+++ src/main/java/com/duckblade/runelite/summergarden/SummerGardenPlugin.java
@@ -3,19 +3,23 @@ package com.duckblade.runelite.summergarden;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.Player;
+import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.client.Notifier;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+
+import java.awt.*;
 
 @Slf4j
 @PluginDescriptor(
@@ -30,12 +34,21 @@ public class SummerGardenPlugin extends Plugin
 	private OverlayManager overlayManager;
 
 	@Inject
+	private Notifier notifier;
+
+	@Inject
 	private ElementalCollisionDetector collisionDetector;
 
 	@Inject
 	private SummerGardenOverlay overlay;
 
+	@Inject
+	private SummerGardenConfig config;
+
 	private static final WorldPoint GARDEN = new WorldPoint(2915, 5490, 0);
+	private static final String STAMINA_MESSAGE = "[One Click Summer Garden] Low Stamina Warning";
+
+	private boolean sentNotification = false;
 
 	@Override
 	protected void startUp() throws Exception
@@ -80,6 +93,18 @@ public class SummerGardenPlugin extends Plugin
 			.stream()
 			.filter(ElementalCollisionDetector::isSummerElemental)
 			.forEach(npc -> collisionDetector.updatePosition(npc, client.getTickCount()));
+
+		// check for stamina usage
+		int stamThreshold = config.staminaThreshold();
+		if (stamThreshold != 0) {
+			boolean stamActive = client.getVar(Varbits.RUN_SLOWED_DEPLETION_ACTIVE) != 0;
+			if (client.getEnergy() <= stamThreshold && !stamActive && !sentNotification) {
+				notifier.notify(STAMINA_MESSAGE, TrayIcon.MessageType.WARNING);
+				sentNotification = true;
+			} else if (client.getEnergy() > stamThreshold) {
+				sentNotification = false;
+			}
+		}
 	}
 
 	@Provides
